5c97f79f6099185db391665e102d2a3b8c056a2d
thefenry <thefenry@gmail.com>
2014-04-02 20:45:56 -0700

    Add rails skeleton and remove add database yml to git ignore

---
+++ minichat/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Minichat::Application.config.secret_key_base = '937b50c705ed9b70ee95ebd7666e5c3e22faf7e31a7015c226f4ccf5f31e8dd8d366d844f2a22a6d0ca82a3f635bac54d2305df91926c398af4a803d168ab444'
