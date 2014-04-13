2017dd8378bb0111365e44d58dc3443482193950
thefenry <thefenry@gmail.com>
2014-04-13 09:37:09 -0700

    Add user controller test for creation

--- minichat/spec/controllers/users_controller_spec.rb
+++ minichat/spec/controllers/users_controller_spec.rb
@@ -4,7 +4,24 @@ describe UsersController do
 
 	context '#new' do
 		it 'should load login page' do 
+			get :new
+			expect(response).to be_ok
+		end
 
+		it 'should assign @user to new user'
+	end
+
+	context '#create' do
+		context 'with valid attributes' do
+			it 'should redirect user'
+
+			it 'should increate User count by one'
+		end
+		
+		context 'with invalid attributes' do
+			it 'should not redirect'
+
+			it 'should not increase user count by one'
 		end
 	end
 end
