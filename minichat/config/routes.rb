9a818d6c273d63769c7833a29ef2be8a09fcecc8
thefenry <thefenry@gmail.com>
2014-04-13 09:49:30 -0700

    Remove message routes that are not used and only add nessesary routes for userscontroller

--- minichat/config/routes.rb
+++ minichat/config/routes.rb
@@ -1,5 +1,5 @@
 Minichat::Application.routes.draw do
   root :to => 'messages#index'
-  resources :messages
-  
+  resources :messages, only: [:index, :create]
+  resources :users, only: [:new, :create]
 end
