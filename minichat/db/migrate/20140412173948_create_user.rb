58aadac7a13c654c74b8b0a7bac1b406f787f4d6
thefenry <thefenry@gmail.com>
2014-04-13 09:44:46 -0700

    Run migration for new users table

--- minichat/db/migrate/20140412173948_create_user.rb
+++ minichat/db/migrate/20140412173948_create_user.rb
@@ -3,7 +3,7 @@ class CreateUser < ActiveRecord::Migration
     create_table :users do |t|
     	t.string :username
     	t.string :password_digest
-    	t.email :email
+    	t.string :email
     	t.timestamps
     end
   end
