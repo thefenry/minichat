665d97752d695ca69cfb87e1de4c34a5347345d4
thefenry <thefenry@gmail.com>
2014-04-15 22:26:45 -0700

    Add relationship between user and messages

--- minichat/app/models/user.rb
+++ minichat/app/models/user.rb
@@ -2,4 +2,5 @@ class User < ActiveRecord::Base
 	has_secure_password
 	validates :email, :username, :password_digest, :presence => true
 	validates :email, :username, :uniqueness => true
+	has_many :messages
 end
