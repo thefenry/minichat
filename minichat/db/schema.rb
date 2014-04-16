a6e8e659c067b9786a2d7881943bc1de5a648d65
thefenry <thefenry@gmail.com>
2014-04-15 22:25:07 -0700

    Add new references between user and messages

--- minichat/db/schema.rb
+++ minichat/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140412173948) do
+ActiveRecord::Schema.define(version: 20140416042655) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,7 +20,10 @@ ActiveRecord::Schema.define(version: 20140412173948) do
     t.string   "content"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "user_id"
   end
+
+  add_index "messages", ["user_id"], name: "index_messages_on_user_id", using: :btree
 
   create_table "users", force: true do |t|
     t.string   "username"
